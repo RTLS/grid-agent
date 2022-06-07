989e67164e7e5887daeed7476bad6c3e050ba6c1
RTLS <carter.e.pedersen@gmail.com>
2022-06-06 14:32:28 -1000

    Make agent move in update loop

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@ extern crate glutin_window;
 extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
+extern crate rand;
 
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
+
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng
+};
+
 
 const WIDTH: f64 = 1000.0;
 const HEIGHT: f64 = 1000.0;
@@ -16,12 +23,57 @@ const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 const ENGLISH_VERMILLION: [f32; 4] = [211.0/256.0, 62.0/256.0, 67.0/256.0, 1.0];
 const OLD_LAVENDER: [f32; 4] = [102.0/256.0, 99.0/256.0, 112.0/256.0, 1.0];
 
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Distribution<Direction> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Direction {
+        match rng.gen_range(0..=3) {
+            0 => Direction::Up,
+            1 => Direction::Down,
+            2 => Direction::Left,
+            _ => Direction::Right,
+        }
+    }
+}
+
 pub struct Position {
     x: u32, y: u32
 }
 
 pub struct Agent {
     position: Position,
+}
+
+impl Agent {
+    fn step(&mut self, dir: Direction, grid: graphics::grid::Grid) {
+        match dir {
+            Direction::Up => {
+                if self.position.y != 0 {
+                    self.position.y -= 1;
+                }
+            },
+            Direction::Down => {
+                if self.position.y != grid.rows - 1 {
+                    self.position.y += 1;
+                }
+            },
+            Direction::Left => {
+                if self.position.x != 0 {
+                    self.position.x -= 1;
+                }
+            },
+            Direction::Right => {
+                if self.position.x != grid.cols - 1 {
+                    self.position.x += 1;
+                }
+            },
+        }
+    }
 }
 
 pub struct App {
@@ -55,7 +107,9 @@ impl App {
     }
 
     fn update(&mut self, _args: &UpdateArgs) {
-        // TODO
+        let mut rng = rand::thread_rng();
+        let direction: Direction = rng.gen();
+        self.agent.step(direction, self.grid);
     }
 }
 
