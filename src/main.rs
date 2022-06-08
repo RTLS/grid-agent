fcfa35c4bf8897235ce819b6113594bca7b9a0f2
RTLS <carter.e.pedersen@gmail.com>
2022-06-07 14:22:20 -1000

    Spawn food on grid

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
-
+use std::vec::Vec;
 use rand::{
     distributions::{Distribution, Standard},
     Rng
 };
 
 
-const WIDTH: f64 = 1000.0;
+const WIDTH: f64 = 1500.0;
 const HEIGHT: f64 = 1000.0;
 
 const UPDATES_PER_SECOND: u64 = 16;
@@ -25,7 +25,9 @@ const MAX_SPEED: bool = false;
 const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 const ENGLISH_VERMILLION: [f32; 4] = [211.0/256.0, 62.0/256.0, 67.0/256.0, 1.0];
 const OLD_LAVENDER: [f32; 4] = [102.0/256.0, 99.0/256.0, 112.0/256.0, 1.0];
+const FERN_GREEN : [f32; 4] = [88.0/256.0, 129.0/256.0, 87.0/256.0, 1.0];
 
+const INITIAL_FOOD_COUNT: u32 = 5000;
 const AGENT_STEP_ENERGY_COST: f64 = 0.05;
 
 pub enum Direction {
@@ -90,10 +92,21 @@ pub struct Food {
     energy: f64,
 }
 
+impl Food {
+    fn new_in(grid: &graphics::grid::Grid) -> Food {
+        let mut rng = rand::thread_rng();
+        let x = rng.gen_range(0..grid.cols) as i32;
+        let y = rng.gen_range(0..grid.rows) as i32;
+
+        Food{position: Position{x, y}, energy: 1.0}
+    }
+}
+
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
     grid: graphics::grid::Grid,  // Grid
-    agent: Agent
+    agent: Agent,
+    food: Vec<Food>,
 }
 
 impl App {
@@ -108,6 +121,9 @@ impl App {
         let x = self.agent.position.x as f64 * self.grid.units;
         let y = self.agent.position.y as f64 * self.grid.units;
 
+        // Food
+        let food = &self.food;
+
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
             clear(OLD_LAVENDER, gl);
@@ -117,6 +133,14 @@ impl App {
 
             //Agent
             rectangle(ENGLISH_VERMILLION, square, c.transform.trans(x, y), gl);
+
+            // Food
+            for food in food {
+                let square = rectangle::square(0.0, 0.0, self.grid.units);
+                let x = food.position.x as f64 * self.grid.units;
+                let y = food.position.y as f64 * self.grid.units;
+                rectangle(FERN_GREEN, square, c.transform.trans(x, y), gl);
+            }
         });
     }
 
@@ -141,13 +165,30 @@ impl App {
     }
 
     fn valid_position(&mut self, position: &Position) -> bool {
-        self.in_bounds(position)
+        let in_bounds = self.in_bounds(position);
+        let agent_position = position.x == self.agent.position.x && position.y == self.agent.position.y;
+        let food_at_position = self.food.iter().any(|f| f.position.x == position.x && f.position.y == position.y);
+
+        in_bounds && !agent_position && !food_at_position
     }
 
     fn in_bounds(&self, position: &Position) -> bool {
         let rows = self.grid.rows as i32;
         let cols = self.grid.cols as i32;
         position.x >= 0 && position.y >= 0 && position.x < cols - 1 && position.y < rows - 1
+    }
+
+    fn generate_initial_food(&mut self) {
+       self.food = Vec::with_capacity(INITIAL_FOOD_COUNT as usize);
+       let mut food_count: u32 = 0;
+
+       while food_count < INITIAL_FOOD_COUNT {
+           let food = Food::new_in(&self.grid);
+           if self.valid_position(&food.position) {
+                self.food.push(food);
+                food_count += 1;
+           }
+       }
     }
 }
 
@@ -167,7 +208,10 @@ fn main() {
         gl: GlGraphics::new(opengl),
         grid: graphics::grid::Grid{cols: 100, rows: 100, units: 10.0},
         agent: Agent::new(),
+        food: Vec::new(),
     };
+
+    app.generate_initial_food();
 
     let mut event_settings = EventSettings::new();
     event_settings.max_fps = UPDATES_PER_SECOND;
